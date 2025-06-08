ae492aee05344bb3678346706f8c3df21d9853cb
Alafuwa <alafuwa@gmail.com>
2025-06-08 14:18:08 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN7LRFzF5nJVgL...",
-  authDomain: "alafuwa-data-app.firebaseapp.com",
-  projectId: "alafuwa-data-app",
-  storageBucket: "alafuwa-data-app.appspot.com",
-  messagingSenderId: "851776851925",
-  appId: "1:851776851925:web:f0f08f122d32665f3ed413",
-  measurementId: "G-CQCSWPPQXR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
